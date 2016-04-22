4b375d74ef27edfeb69c18563a8ab830a18be977
Ricardo Mendes <rokusu@gmail.com>
2016-04-22 13:26:08 +0100

    Adds git push strategy

--- lib/middleman-deploy/strategies.rb
+++ lib/middleman-deploy/strategies.rb
@@ -1,3 +1,4 @@
 require 'middleman-deploy/strategies/git/base'
+require 'middleman-deploy/strategies/git/push'
 require 'middleman-deploy/strategies/git/force_push'
 require 'middleman-deploy/strategies/git/submodule'
